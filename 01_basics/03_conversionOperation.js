a2bcde194ba1146832ac006da4642428602dca92
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-03 23:44:56 +0530

    added conversion operation js file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+console.log("Third Day of JavaScript!");
+
+
+let score = "33"
+let score2 = 33
+
+console.log(score);
+console.log(typeof(score));
+console.log(score2);
+console.log(typeof(score2));
+
+scoret = Number("amol");
+console.log(`scoret is : ${scoret}`);
+console.log(typeof scoret );
+
