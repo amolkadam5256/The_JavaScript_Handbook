4a6009ec949d450cbdca62e3ac0598e89c8bfe8c
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-03 23:12:41 +0530

    Pratic some Code and pratic to push and commit code on github

--- 01_basics/01_basics.js
+++ 01_basics/01_basics.js
@@ -1,2 +1,11 @@
 console.log("Hello World");
 console.log("Welcome to JavaScript tutorial");
+
+const accountId = 770926;
+let accountEmail = "amolkadma1234";
+var accountPassword = 776810;
+let accountCity = "pune";
+
+console.table([
+  { "account id": accountId, "email": accountEmail, "password": accountPassword, "city": accountCity }
+]);
