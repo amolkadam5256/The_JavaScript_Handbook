4aaee621f8e02826bcb0ddd8585e96b9ed7c837b
amolkadam5256 <amolkadam1274@gmail.com>
2025-03-16 11:48:40 +0530

    Mini Project 3 creating ColorCrafter - Color Changer App

---
+++ 05_MiniProjects/03_ColorCrafter/script.js
@@ -0,0 +1,9 @@
+function changeBgColor() {
+    const bg = document.getElementById("backgroundColorPicker").value;
+    document.body.style.backgroundColor = bg;
+}
+
+function changeTextColor() {
+    const text = document.getElementById("textColorPicker").value;
+    document.body.style.color = text;
+}
